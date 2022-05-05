cf674a814d2e66526121e656bc12167fc3028cb1
Elie <61464021+C-B-Elite@users.noreply.github.com>
2022-05-05 16:34:53 +0800

    Delete src/declarations/withdraw directory

--- src/declarations/withdraw/withdraw.did.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export interface CycleActor { 'withdraw_cycles' : () => Promise<undefined> }
-export interface _SERVICE extends CycleActor {}
--- src/declarations/withdraw/withdraw.did.js
+++
@@ -1,5 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  const CycleActor = IDL.Service({ 'withdraw_cycles' : IDL.Func([], [], []) });
-  return CycleActor;
-};
-export const init = ({ IDL }) => { return []; };
